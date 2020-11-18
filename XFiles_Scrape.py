6d8ea9bc5e26d0fee0e9431d8f1575f9b07b6b52
Kyle-KJ <kknightjenkins@gmail.com>
2020-11-18 17:07:16 +0000

    Transcript scraper updated

--- XFiles_Scrape.py
+++ XFiles_Scrape.py
@@ -9,7 +9,7 @@ req = requests.get(URL)
 html = req.content
 soup = BeautifulSoup(html, 'lxml')
 
-xfiles_data = []
+xfiles_eps = []
 
 tables = soup.find_all('table')
 
@@ -28,8 +28,93 @@ for table in tables:
             ep_aired = cols[2].string
 
             ep_data = [ep_trans, ep_name, ep_num, ep_aired]
-            xfiles_data.append(ep_data)
-            print(ep_data)
-            
-            
+            xfiles_eps.append(ep_data)
 
+# Loop through transcripts
+
+xfiles_lines = []
+xfiles_scenes = []
+
+xfiles_eps = xfiles_eps[5:7]
+
+for ep in xfiles_eps:
+
+    ep_url = URL + ep[0]
+    ep_id = ep[2]
+    print("Loading Episode: " + ep[1])
+
+    tr_html = requests.get(ep_url).content
+    tr_soup = BeautifulSoup(tr_html, 'lxml')
+
+    body = tr_soup.find('body')
+    body = str(body)
+    body_split = body.rsplit("[THE END]")[0]
+    scenes = body_split.split("SCENE ")[1:]
+
+    for scene in scenes:
+
+        scene_details = scene.split("<p>")[0].rsplit("<br/>")
+        scene_num = "Scene " + scene_details[0].strip()
+        scene_desc = ''.join(scene_details[1:]).replace('\n', ' ').strip()
+
+        if '(' in scene_desc:
+            context = scene_desc.split('(')[1].split(')')[0]
+            context = '(' + context + ')'
+
+            caught_data = [ep_id, scene_num, "Context", "", context]
+            xfiles_lines.append(caught_data)
+            
+            scene_desc = scene_desc.split('(')[0] + scene_desc.split(')')[1]
+        
+        if '<b>' in scene_desc:
+            s_text = scene_desc.split('<b>')[1].split(':</b>')
+            s_char = s_text[0]
+            s_line = s_text[1].strip()
+
+            caught_data = [ep_id, scene_num, "Line", s_char, s_line]
+            xfiles_lines.append(caught_data)
+
+            scene_desc = scene_desc.split('<b>')[0]
+        
+        scene_desc = scene_desc.strip()
+
+        scene_data = [ep_id, scene_num, scene_desc]
+
+        xfiles_scenes.append(scene_data)
+
+        scene_soup = BeautifulSoup(scene, 'lxml')
+        paras = scene_soup.find_all('p')[1:]
+
+        for para in paras:
+
+            try:
+                char_name = para.find('b').text
+                char_name = char_name.split(':')[0] # Some bad data will be picked up here - consider filtering for names that appear more than once
+                line_text = str(para).split('</b>')[1:]
+                line_text = ''.join(line_text)
+                line = line_text.replace('\n', ' ').split('</p>')[0].strip()
+                line_type = "Line"
+
+                line_data = [ep_id, scene_num, line_type, char_name, line]
+
+                xfiles_lines.append(line_data)
+
+            except:
+                line_text = para.get_text()
+                line = line_text.replace('\n', ' ').strip()
+                line_type = "Context"
+
+                line_data = [ep_id, scene_num, line_type, '', line]
+
+                if line != '':
+                    xfiles_lines.append(line_data)
+
+
+print("\nLines Sample:")        
+for i in range(15):
+    print(xfiles_lines[i])
+
+
+print("\nScenes Sample:")
+for i in range(12):
+    print(xfiles_scenes[i])
